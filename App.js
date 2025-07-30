981edc974bc1642af76e4e490978cb683a465218
dev11 <dev11@saluberdev.com>
2025-07-30 10:51:41 +0300

    added card component

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
+import Card from './app/components/Card'
+
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View style={{backgroundColor: '#f8f4f4', padding: 20, paddingTop: 100}}>
+      <Card title="Red Jacket For Sale" subtitle="$100" image={require("./app/assets/jacket.jpg")}/>
+    </View>
   );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Platform, Text, StyleSheet } from 'react-native';
 
-function AppText({children}) {
+function AppText({children, style}) {
     return (
-        <Text style={styles.text}>{children}</Text>
+        <Text style={[styles.text, style]}>{children}</Text>
     );
 }
 

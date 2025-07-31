d29607b25c6957635594a8ce5e7be74b9cbf2790
dev11 <dev11@saluberdev.com>
2025-07-31 09:41:42 +0300

    end of part 1

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
@@ -18,7 +19,7 @@ import LoginScreen from "./app/screens/LoginScreen";
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <LoginScreen />
+      <ListingEditScreen />
     </GestureHandlerRootView>
   );
 }
